65431457ce115c35271f9d93f0745ba9b606ab8d
Joseph Lee <jlee@mn8.gateway.sonic.net>
2014-05-26 18:41:11 -0700

    Initial Commit

---
+++ tipcalculator/JLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JLAppDelegate.h
+//  tipcalculator
+//
+//  Created by Joseph Lee on 5/26/14.
+//  Copyright (c) 2014 mn8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
